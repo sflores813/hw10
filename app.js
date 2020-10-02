f99ac2536ed808fc433083a645efabb3a406babc
Stephen Flores <sflores813@gmail.com>
2020-10-02 14:16:36 -0500

    cleaned up code, added function to create team.html

--- app.js
+++ app.js
@@ -4,140 +4,151 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+// where the team array will be stored
 const team = [];
+// const to create the output folder
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-const prompt = [{
-    type: "list",
-    name: "employee",
-    message: "Who are you adding to your team?",
-    choices: ["Manager","Engineer", "Intern","finish team setup"]
-}];
-//manager questions
-const mQestions = [{
+//---adding more members to the team.
+function teamCreate() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "What team member would you like to add",
+        choices: ["Manager", "Engineer", "Intern", "finish creating team."],
+      },
+    ])
+    .then(function (response) {
+      if (response.role === "Manager") {
+        createTeamManager();
+      } else if (response.role === "Engineer") {
+        createEngineer();
+      } else if (response.role === "Intern") {
+        createIntern();
+      } else {
+        makeTeam();
+      }
+    });
+}
+//---creating the manager information.
+function createTeamManager() {
+  inquirer
+    .prompt([
+      {
         type: "input",
         name: "name",
-        message: "What is the team mangers name?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "what is the team managers email?"
-    },
-    {
+        message: "What is the team managers name?",
+      },
+      {
         type: "input",
         name: "id",
-        message: "employee id number?."
-    },
-    {
+        message: "what is the managers ID number?",
+      },
+      {
         type: "input",
-        name: "OfficeNumber",
-        message: "what is youre office?."
-    }
-
-];
-
-// engineer questions
-const eQuestions = [{
-    type: "input",
-    name: "name",
-    message: "What is the engineers name?"
-},
-{
-    type: "input",
-    name: "email",
-    message: "What is the engineers email?"
-},
-{
-    type: "input",
-    name: "id",
-    message: "What is the engineers employee ID number?"
-},
-{
-    type: "input",
-    name: "github",
-    message: "What is your GitHub username?"
+        name: "email",
+        message: "what is the managers email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the managers office number?",
+      },
+    ])
+    .then(function (response) {
+      const manager = new Manager(
+        response.name,
+        response.id,
+        response.email,
+        response.officeNumber
+      );
+      team.push(manager);
+      teamCreate();
+    });
 }
-];
-
-// intern questions
-
-const iQuestions = [{
+//---creating engineer information.
+function createEngineer() {
+  inquirer
+    .prompt([
+      {
         type: "input",
         name: "name",
-        message: "What is the interns name?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "what is  the intern email?"
-    },
-    {
+        message: "What is the engineers name?",
+      },
+      {
         type: "input",
         name: "id",
-        message: "interns employee ID number?"
-    },
-    {
+        message: "What is the engineers ID?",
+      },
+      {
         type: "input",
-        name: "school",
-        message: "where did the intern graduate college from?"
-    }
-];
-
-
-function init() {
-    inquirer.prompt(prompt)
-        .then(function (data) {
-            switch (data.employee) {
-                case "Manager":
-                    manager()
-                    break;
-                    case "Engineer":
-                        engineer()
-                        break;
-                case "Intern":
-                    intern()
-                    break;
-                case "Finish team setup":
-                    render(team)
-                    break;
-                }
-            });
-        }
-              
-
-// setting up the manager data
-function manager() {
-    inquirer.prompt(mQestions)
-        .then(function (data) {
-            const newManager = new Manager(data.name, data.email, data.id, data.OfficeNumber)
-            team.push(newManager)
-            init()
-        })
+        name: "email",
+        message: "What is the engineers email?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the engineers github user name?",
+      },
+    ])
+    .then(function (response) {
+      const engineer = new Engineer(
+        response.name,
+        response.id,
+        response.email,
+        response.github
+      );
+      team.push(engineer);
+      teamCreate();
+    });
 }
-// setting up engineer data
-function engineer() {
-    inquirer.prompt(eQuestions)
-        .then(function (data) {
-            const newEngineer = new Engineer(data.name, data.email, data.id, data.github)
-            team.push(newEngineer)
-            init()
-        })
+//--Collecting information on the intern team memmber.
+function createIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the interns name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the interns ID number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the interns email?",
+      },
+      {
+        type: "input",
+        name: "schoolName",
+        message: "Where did the intern go to school",
+      },
+    ])
+    .then(function (response) {
+      const intern = new Intern(
+        response.name,
+        response.id,
+        response.email,
+        response.schoolName
+      );
+      team.push(intern);
+      teamCreate();
+    });
 }
-// setting up intern data
-function intern() {
-    inquirer.prompt(iQuestions)
-        .then(function (data) {
-            const newIntern = new Intern(data.name, data.email, data.id, data.school)
-            team.push(newIntern)
-            init()
-        })
+//--Creating HTML based on the information provided by the user.
+function makeTeam() {
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  fs.writeFileSync(outputPath, render(team), "utf-8");
 }
-init();
 
-
-
+createTeamManager();
